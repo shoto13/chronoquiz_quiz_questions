ecc828eae2e35f49ef75ae432cc00713c69de704
shoto13 <wilsonrc@pm.me>
2024-06-10 13:24:10 +0100

    Seiko Questions mk1

---
+++ seikoQuestions.js
@@ -0,0 +1,652 @@
+export default [
+  {
+    question: "What type of movement is in the Grand Seiko SGBA211 Snowflake?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"quartz",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"kinetic",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"spring drive",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"automatic"
+      },
+    ],
+    correctAnswerIndex:2
+  },
+  {
+    question: "In what year did Seiko introduce its first diver's watch?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"1959",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"1965",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"1970",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"1985"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "What does the Seiko '5' in the Seiko 5 series stand for?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"five-year warranty",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"five key atttributes",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"five-hand movement",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"five time zones"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "When did Seiko introduce its Spring Drive technology?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"1997",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"2001",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"2005",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"2010"
+      },
+    ],
+    correctAnswerIndex:0
+  },
+  {
+    question: "Which Seiko collection is specifically designed for professional divers?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"seiko 5",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"presage",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"prospex",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"astron"
+      },
+    ],
+    correctAnswerIndex:2
+  },
+  {
+    question: "What year was Seiko founded?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"1913",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"1901",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"1877",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"1881"
+      },
+    ],
+    correctAnswerIndex:3
+  },
+  {
+    question: "Who founded Seiko?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Seiji Saito",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Kintaro Hattori",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Kenji Yoshida",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Jiro Tanaka"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "What was Seiko’s first wristwatch called?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Seikosha",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Presage",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Grand seiko",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Laurel"
+      },
+    ],
+    correctAnswerIndex:3
+  },
+  {
+    question: "In which year did Seiko introduce the world’s first quartz wristwatch?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"1963",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"1969",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"1975",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"1980"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "What is the significance of Seiko's 'Diashock' technology?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Increases accuracy",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Enhances water resistance",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Improves shock resistance",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Reduces glare"
+      },
+    ],
+    correctAnswerIndex:2
+  },
+  {
+    question: "In which city is Seiko's headquarters located?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Kyoto",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Osaka",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Hiroshima",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Tokyo"
+      },
+    ],
+    correctAnswerIndex:3
+  },
+  {
+    question: "What year did Seiko introduce the 'Magic Lever' for automatic winding?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"1948",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"1959",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"1963",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"1972"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "Which Seiko watch model was worn by Martin Sheen in the movie 'Apocalypse Now'?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Seiko 6105",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Seiko 6139",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Seiko 6309",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Seiko 7A28"
+      },
+    ],
+    correctAnswerIndex:0
+  },
+  {
+    question: "In which year did Seiko release its first GPS solar watch?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"2000",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"2005",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"2012",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"2017"
+      },
+    ],
+    correctAnswerIndex:2
+  },
+  {
+    question: "Seiko's 'Zaratsu' polishing technique is most associated with which collection?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Presage",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Prospex",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Astron",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Grand Seiko"
+      },
+    ],
+    correctAnswerIndex:3
+  },
+  {
+    question: "Which Seiko collection features enamel and porcelain dials inspired by traditional Japanese craftsmanship?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Astron",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Prospex",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Presage",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Seiko 5"
+      },
+    ],
+    correctAnswerIndex:2
+  },
+  {
+    question: "What is Seiko's high-beat movement frequency for Grand Seiko watches?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"21,600 vph",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"28,800 vph",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"36,000 vph",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"43,200 vph"
+      },
+    ],
+    correctAnswerIndex:2
+  },
+  {
+    question: "What special feature does Seiko's 'Tuna' dive watch have?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"A helium escape valve",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"An external shroud",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"A rotating bezel",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"A ceramic case"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "What is the power reserve of Seiko's Spring Drive movement?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"24 hours",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"48 hours",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"100 hours",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"72 hours"
+      },
+    ],
+    correctAnswerIndex:3
+  },
+  {
+    question: "Which Seiko model is nicknamed the 'Arnie' due to its appearance in a movie?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Seiko 6105",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Seiko H558",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Seiko 7A28",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Seiko SKX007"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "What is the depth rating of the Seiko Marinemaster (SBDX001)?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"300 Metres",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"200 Metres",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"100 Metres",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"600 Metres"
+      },
+    ],
+    correctAnswerIndex:0
+  },
+  {
+    question: "What feature does the Seiko 'Flightmaster' offer?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Dive timing bezel",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Slide rule bezel",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"GMT hand",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Tachymeter"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "Which Seiko model is associated with the nickname 'Turtle'?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Seiko 6105",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Seiko SRP777",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Seiko SKX007",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Seiko Sumo"
+      },
+    ],
+    correctAnswerIndex:1
+  },
+  {
+    question: "What is the typical battery life of a Seiko solar watch?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"6 months",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"1 year",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"2 years",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"10 years"
+      },
+    ],
+    correctAnswerIndex:3
+  },
+  {
+    question: "What is the nickname of the Seiko SKX009?",
+    options: [
+      {
+        id:"0",
+        options:"A",
+        answer:"Batman",
+      },
+      {
+        id:"1",
+        options:"B",
+        answer:"Hulk",
+      },
+      {
+        id:"2",
+        options:"C",
+        answer:"Pepsi",
+      },
+      {
+        id:"3",
+        options:"D",
+        answer:"Coke"
+      },
+    ],
+    correctAnswerIndex:2
+  },
+]
